8995b3edc28f2b829830712e45fbf0187824ce9e
kuba <smolagakuba@gmail.com>
2020-03-27 00:05:56 +0100

    complete rewrite

--- index.js
+++ index.js
@@ -6,22 +6,74 @@ if (process.argv.length !== 4) {
 const fs = require('fs')
 const inputDir = process.argv[2]
 const outputFile = process.argv[3]
-let output = ''
 
-let fileNames = fs.readdirSync(inputDir)
-for (let i = 0; i < fileNames.length; i++) {
-  let fileName = fileNames[i]
-  let data = fs.readFileSync(inputDir + '/' + fileName).toString()
-  let prefix = fileName.slice(0, -4) + '__'
-  let previousPos = 0
-  let charPos = 0
-  while (charPos < data.length - 1) {
-    while (charPos < data.length - 1 && data[charPos] !== '.') charPos++
-    if (charPos === data.length - 1) break
-    output += data.slice(previousPos, charPos + 1) + prefix
-    previousPos = charPos + 1
-    while (data[charPos] !== '}') charPos++
-  }
-  output += data.slice(previousPos, charPos + 1) + '\n\n'
+let output = ''
+let data, prefix, charPos, previousPos
+
+const currentChar = () => data[charPos]
+const isWhitespace = c => (c === ' ') || (c === '\n') || (c === '\t')
+const consumeWhitespace = () => consumeWhile(isWhitespace)
+const consumeComment = () => consumeWhile(() => data.substr(charPos, 2) !== '*/')
+
+const skipWhile = (condition) => {
+  while (condition(currentChar())) charPos++
 }
-fs.writeFileSync(outputFile, output)
+
+const consumeWhile = (condition) => {
+  skipWhile(condition)
+  consume()
+}
+
+const consume = () => {
+  output += data.slice(previousPos, charPos)
+  previousPos = charPos
+}
+
+const consumeChar = () => {
+  charPos++
+  consume()
+}
+
+const consumeClassBlock = () => {
+  consumeChar() // eat the dot
+  output += prefix
+  consumeWhile(c => c !== '}')
+  consumeChar() // eat the "}"
+}
+
+const consumeMediaBlock = () => {
+  consumeWhile(c => c !== '{')
+  consumeChar()
+  parseUntil(c => c === '}')
+  consumeChar()
+}
+
+const consumeTagBlock = () => {
+  consumeWhile(c => c !== '}')
+  consumeChar()
+}
+
+const parseUntil = (condition) => {
+  consumeWhitespace()
+  if (condition(currentChar())) return undefined
+  if (currentChar() === '/') consumeComment()
+  else if (currentChar() === '.') consumeClassBlock()
+  else if (currentChar() === '@') consumeMediaBlock()
+  else consumeTagBlock()
+  parseUntil(condition)
+}
+
+const setup = (filename) => {
+  data = fs.readFileSync(inputDir + '/' + filename).toString()
+  prefix = filename.slice(0, -4) + '__'
+  previousPos = 0
+  charPos = 0
+}
+
+fs.readdirSync(inputDir).forEach(filename => {
+  setup(filename)
+  parseUntil(c => typeof c === 'undefined')
+  output += '\n\n'
+})
+
+fs.writeFileSync(outputFile, output.trimRight())
